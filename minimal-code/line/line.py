783dd71be6e5d8582feadea6adfc3913f62583e7
Boboaca Costel <bobyca2003@yahoo.com>
2023-12-10 16:30:58 +0100

    Update line.py

--- minimal-code/line/line.py
+++ minimal-code/line/line.py
@@ -9,8 +9,8 @@ class tutorial(inkex.EffectExtension):
 
     def effect(self):
         
-        self.svg.add(Line(x1='0', y1= '0', x2='100', y2='100'))
-        line1.style = {'stroke-width': 3, 'stroke': 'blue'}  
+        line=self.svg.add(Line(x1='0', y1= '0', x2='100', y2='100'))
+        line.style = {'stroke-width': 3, 'stroke': 'blue'}  
 		
 if __name__ == '__main__':
     tutorial().run()
